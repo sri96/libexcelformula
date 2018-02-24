9aa1b82e7f28e3710cbaeaed96c88bffda2c45f3
Sri Madhavi Rajasekaran <adhirai96@gmail.com>
2018-02-24 13:01:23 -0800

    Moving all the unit test files to the unit_test folder inside of the test folder. This is to make room for adding the performance tests to the test folder and to avoid confusion

--- test/unit_test/unit_test_driver.cpp
+++ test/unit_test/unit_test_driver.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "../lib/catch/catch.hpp" // TODO - Add include paths to CMake to avoid using relative paths
+#include "../../thirdparty/catch/catch.hpp" // TODO - Add include paths to CMake to avoid using relative paths
 #include "list_of_ut_folders.hpp" // Contains a list of .hpp files that hold the actual UTs from each folder
 
 
