49e4f6121a0d5a9036ecc7829d1b1f80f5795b60
Sri Madhavi Rajasekaran <adhirai96@gmail.com>
2018-02-24 02:06:35 -0800

    Adding the ut infrastructure using CMake and Catch. The unit test driver is called unit_test_driver.cpp and it includes the list_of_ut_folders.hpp. list_of_ut_folders.hpp actually will contain more .hpp files - each one belonging to a folder inside the test folder. Those .hpp files will further contain more .hpp files that actually contain the unit tests. I understand it is a complex architecture. But, I am trying to minimize the number of files you have to touch to add unit tests and also they will all be run at the same time. This should prevent worries of making changes in one part of the code and accidentally breaking some other part that you didn't know you had a dependency on. There are no unit tests currently. I will be adding more as I start writing the code

---
+++ test/list_of_ut_folders.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef LIST_OF_UT_FOLDERS_HPP
+#define LIST_OF_UT_FOLDERS_HPP
+
+// This file is currently empty. As we add new folders, this will have a list of all .hpp files from each of the 
+// unit test folders
+
+#endif
---
+++ test/unit_test_driver.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "../lib/catch/catch.hpp" // TODO - Add include paths to CMake to avoid using relative paths
+#include "list_of_ut_folders.hpp" // Contains a list of .hpp files that hold the actual UTs from each folder
+
+
+// This is an empty file by design. This cpp file only provides the driver for running UTs. Actual UTs
+// are found inside folders.
