fb78319b23c94ec198f2d63934211045549569bf
Sri Madhavi Rajasekaran <adhirai96@gmail.com>
2018-02-24 19:06:39 -0800

    Somehow missed checking in the IExcelFormulaToken alongside the Lexer changes. The lexer actually exports a vector of tokens each of which is a derivative of IExcelFormulaToken

---
+++ src/shared/i_excel_formula_token.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef I_EXCEL_FORMULA_TOKEN_H
+#define I_EXCEL_FORMULA_TOKEN_H
+
+namespace ExcelFormula
+{
+    // Both the Lexer and Parser needs to be aware of tokens
+    class IExcelFormulaToken
+    {
+        // Currently an empty interface. Will be adding things here shortly
+    };
+};
+
+#endif
