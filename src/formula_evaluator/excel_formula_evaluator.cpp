70404d12f5e6620ea1cd20c98c95aa442bd8f017
Sri Madhavi Rajasekaran <adhirai96@gmail.com>
2018-02-24 23:43:56 -0800

    Adding the parsing step to evaluator and adding an error to indicate parsing error. I forgot to add it while adding the parsing component

--- src/formula_evaluator/excel_formula_evaluator.cpp
+++ src/formula_evaluator/excel_formula_evaluator.cpp
@@ -1,9 +1,12 @@
+#include <functional>
+
 #include "excel_formula_evaluator.h"
 #include "../formula_lexer/i_excel_formula_lexer.h"
-#include <functional>
+#include "../formula_parser/i_excel_formula_parser.h"
 
 using namespace ExcelFormula;
 using namespace ExcelFormula::Lexer;
+using namespace ExcelFormula::Parser;
 
 EvaluatedFormulaOutput ExcelFormulaEvaluator::EvaluateFormula(std::wstring_view inputFormulaString, const std::function<void(const std::wstring_view, std::wstring&)>& /*inputCallbackFunction*/) const noexcept
 {
@@ -18,7 +21,15 @@ EvaluatedFormulaOutput ExcelFormulaEvaluator::EvaluateFormula(std::wstring_view 
         output.outputFormulaError = LibExcelFormulaError::LexerError;
         return output;
     }
+
     // 2. Once you have the tokens, you need to parse them into a parse tree. Let's go do that now.
+    const std::unique_ptr<IExcelFormulaParser> excelFormulaParserInstance = CreateExcelFormulaParserInstance();
+    const auto [parserError, parsedFormulaTree] = excelFormulaParserInstance->BuildParseTreeFromTokens(lexedTokens);
+    if (parserError != ParserError::None)
+    {
+        output.outputFormulaError = LibExcelFormulaError::ParserError;
+        return output;
+    }
 
     // 3. Analyze the parse tree and make callbacks to the caller to retrieve data for references and other things
     //    that we don't have data for. Since we have the parse tree, let's analyze and perform callbacks to retrieve the data that we  
