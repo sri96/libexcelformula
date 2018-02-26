37439aadb7a4d1eb817e952534987634cd771e64
Sri Madhavi Rajasekaran <adhirai96@gmail.com>
2018-02-26 01:30:47 -0800

    The runtime implementation is temporarily under hold. So I am just returning a not implemented message instead of an actual error

--- src/parse_tree_executor/excel_formula_parse_tree_executor.cpp
+++ src/parse_tree_executor/excel_formula_parse_tree_executor.cpp
@@ -1,10 +1,12 @@
 #include "excel_formula_parse_tree_executor.h"
+#include <queue>
 
 using namespace ExcelFormula::Runtime;
+using namespace ExcelFormula::Parser;
 
 std::pair<RuntimeError, std::wstring> ExcelFormulaParseTreeExecutor::ExecuteParseTree(ExcelFormula::Parser::ExcelFormulaParseTree& /*inputParseTreeToExecute*/) const noexcept
 {
-    return std::make_pair(RuntimeError::Error, L"");
+    return std::make_pair(RuntimeError::NotImplemented, L"Formula Not Implemented");
 }
 
 std::unique_ptr<IExcelFormulaParseTreeExecutor> ExcelFormula::Runtime::CreateExcelFormulaParseTreeExecutorInstance()
